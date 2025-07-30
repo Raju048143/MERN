3781da2165360aabcd905397322a7462a988306c
RAJU YADAV <rajuyadav91391@gmail.com>
2025-07-30 12:15:08 +0530

    Day 1-> 29/07/2025

---
+++ JS/Hoisting.js
@@ -0,0 +1,12 @@
+console.log(x);
+print();
+console.log(print);
+console.log(getName);
+var x = 10;
+function print() {
+  console.log("Radmos Digital!");
+}
+
+var getName = () => {
+  console.log("Get Name");
+};
