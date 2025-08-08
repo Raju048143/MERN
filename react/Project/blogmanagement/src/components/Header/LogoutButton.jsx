2dab787afb3a10628206a42e79754b6e535b7a04
RAJU YADAV <rajuyadav91391@gmail.com>
2025-08-08 10:53:53 +0530

    Blog Management work on progress

---
+++ react/Project/blogmanagement/src/components/Header/LogoutButton.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useDispatch } from 'react-redux'
+import authService from '../../appwrite/config'
+import { logout } from '../../store/authSlice'
+
+function LogoutButton() {
+    const dispatch = useDispatch()
+    const logoutHandler=()=>{
+        authService.logout()
+        .then(()=>dispatch.apply(logout()))
+    }
+  return (
+    <button className='inline-block px-6 py-2 duration-200 hover:bg-blue-100'>Logout</button>
+  )
+}
+
+export default LogoutButton
---
+++ react/Project/blogmanagement/src/components/index.js
@@ -0,0 +1,7 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutButton from "./Header/LogoutButton";
+import RTE from "./RTE";
+export { Header, Footer, Container, Logo, LogoutButton, RTE };
